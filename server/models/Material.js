075b9bd5cca764e686f28b7e2cef39a9b2ae7401
Abhin Raj R <abhinraj@003@gmail.com>
2025-07-12 02:04:02 +0530

    Created material model

---
+++ server/models/Material.js
@@ -0,0 +1,56 @@
+// models/Material.js
+
+import mongoose from "mongoose";
+
+const materialSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        trim: true,
+        maxlength: 300
+    },
+    fileUrl: {
+        type: String,
+        required: true
+    },
+    fileType: {
+        type: String,
+        enum: ['pdf', 'image'],
+        required: true
+    },
+    program: {
+        type: String,
+        required: true
+    },
+    branch: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 12
+    },
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    upvotes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Material = mongoose.model("Material", materialSchema);
+export default Material;
