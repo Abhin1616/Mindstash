e24f80bab4908cde3caf683ee2adf6efe1489e35
Abhin Raj R <abhinraj@003@gmail.com>
2025-07-12 01:33:50 +0530

    Added programs.js config for university structure

---
+++ server/config/programs.js
@@ -0,0 +1,30 @@
+const PROGRAMS = [
+    {
+        name: "B.Tech",
+        branches: [
+            { name: "CSE", semesters: 8 },
+            { name: "ECE", semesters: 8 },
+            { name: "EEE", semesters: 8 },
+            { name: "ME", semesters: 8 },
+            { name: "CE", semesters: 8 },
+            { name: "IT", semesters: 8 }
+        ]
+    },
+    {
+        name: "M.Tech",
+        branches: [
+            { name: "CSE", semesters: 4 },
+            { name: "ECE", semesters: 4 },
+            { name: "Structural", semesters: 4 },
+            { name: "Thermal", semesters: 4 }
+        ]
+    },
+    {
+        name: "MBA",
+        branches: [
+            { name: "General", semesters: 4 }
+        ]
+    }
+];
+
+export default PROGRAMS;
