eb68872e69a2d24f1fd178b6865c2f45ecbe9fb5
Abhin Raj R <abhinraj2003@gmail.com>
2025-07-30 01:53:23 +0530

    debugging

--- client/src/components/MaterialCard.jsx
+++ client/src/components/MaterialCard.jsx
@@ -41,7 +41,7 @@ const MaterialCard = ({
     const toggleEmail = () => {
         setShowEmail((prev) => !prev);
     };
-
+    console.log(currentUserId, isUploader)
     const formatNumber = (num) => {
         if (num >= 1_000_000) return (num / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
         if (num >= 1_000) return (num / 1_000).toFixed(1).replace(/\.0$/, '') + 'K';
